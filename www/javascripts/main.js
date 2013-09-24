ada4f20c74a24ad3421f5dfe5689b95172810fea
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-24 22:36:41 +0200

    Visual scroll instead of jumps on anchor clicks

--- www/javascripts/main.js
+++ www/javascripts/main.js
@@ -1,1 +1,16 @@
 /* All js here */
+
+$(function(){
+  var $root = $('html, body');
+  $('a').click(function(e) {
+      var href = $.attr(this, 'href').substring(1);
+      var selector = "*[name='" + href + "']";
+      var $elem = $(selector);
+      $root.animate({
+          scrollTop: $elem.offset().top
+      }, 500, function () {
+          window.location.hash = href;
+      });
+      return false;
+  });
+})
