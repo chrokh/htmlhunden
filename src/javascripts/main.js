6a2e9f3e4a394e1651cf9d05f7a372b6b55ca05f
Christopher Okhravi <christopher.okhravi@gmail.com>
2013-09-27 16:19:07 +0200

    Add fixed-position-css example into iframe via javascript, not very pretty I admit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,24 @@
-/* All js here */
+/*
+ * Namespace
+ */
+var lathunden = window.lathunden = {}
+
+
+/*
+ * jQuery Load
+ */
 
 $(function(){
+  lathunden.init();
+})
+
+
+/*
+ * Anchors
+ */
+
+lathunden.Anchors = {}
+lathunden.Anchors.init = function(){
   var $root = $('html, body');
   $('a').click(function(e) {
       var href = $.attr(this, 'href').substring(1);
@@ -13,4 +31,35 @@ $(function(){
       });
       return false;
   });
-})
+}
+
+
+
+/*
+ * Examples
+ */
+
+lathunden.Examples = {};
+lathunden.Examples.CSSPositionFixed = {}
+lathunden.Examples.init = function(){
+  lathunden.Examples.CSSPositionFixed.init();
+}
+lathunden.Examples.CSSPositionFixed.init = function(){
+  var html = '';
+  for(var i=0; i<20; i++){
+    html += '<div style="width:100px; height:100px; background:red; margin: 6px;"></div>';
+  }
+  html += '<div style="width:100px; height:100px; background:blue; position:fixed; left:30px; top:20px;"></div>';
+  $('iframe#example-css-position-fixed')[0].contentWindow.document.write(html);
+}
+
+
+
+/*
+ * Initalizer
+ */
+
+lathunden.init = function(){
+  lathunden.Anchors.init();
+  lathunden.Examples.init();
+}
